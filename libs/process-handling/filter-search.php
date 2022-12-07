25883a048513e57a1a1c027a63fe3357ad281250
chymchym1905 <kien.dauoofman@hcmut.edu.vn>
2022-12-08 05:17:51 +0700

    scuff filtersearch

--- libs/process-handling/filter-search.php
+++ libs/process-handling/filter-search.php
@@ -1,17 +1,21 @@
 <?php
 include ('db_conn.php');
-
+require('../cards_display.php');
 
 
 
 if(isset($_POST["filter"])){
     $province = $_POST["province"];
     $dept = $_POST["speciality"];
+    echo $dept ."    ". $province;
     $s = "select * from doctor where md_dept = '$dept' and md_address = '$province'";
     $res = mysqli_query($conn, $s);
     $num = mysqli_num_rows($res);
-    if($num!=0){
-//        header("Location: ././index.php?page=register&error=Username already taken"); DO SEARCH RESULT PAGE
+    if($num>0){
+        while($row = $res->fetch_assoc()){                                              //DO SEARCH RESULT PAGE
+            card($row['md_name'], $row['md_dept'], $row['md_photo'], $row['md_desc']);  //DO SEARCH RESULT PAGE
+        }                                                                               //DO SEARCH RESULT PAGE
+//        header("Location: ././index.php?page=register&error=Username already taken"); //DO SEARCH RESULT PAGE
     }else{
         echo "No results";
 //        header('location: ././index.php?page=login&message=Register successful!');
