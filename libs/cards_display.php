f5f3dfda79f75c8ae814e69fa7f7951433203d2b
WinstonTNguyen <winstontnguyen@gmail.com>
2022-12-08 14:08:21 +0700

    changed card-display

--- libs/cards_display.php
+++ libs/cards_display.php
@@ -1,28 +1,18 @@
 <?php
 
-function card($product_name, $product_price, $product_image, $product_desc){
+function card($md_id, $md_name, $md_dept, $md_image, $md_loc){
     $element="
-    <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
-        <form action=\"index.php\" method=\"post\">
-            <div class=\"card shadow\">
-                <div>
-                    <img src=\"$product_image\" alt=\"Product\" class=\"img-fluid card-img-top\">
-                </div>
-                <div class=\"card-body\">
-                    <h5 class=\"card-title\">$product_name</h5>
-                    <p class=\"card-text\">
-                        $product_desc
-                    </p>
-                    <h5>
-                        <span class=\"price\">
-                           Khoa $product_price
-                        </span>
-                    </h5>
-                    <a class=\"btn btn-success btn-lg\" href=\"Flights\">Book now!</a>
-                </div>
-            </div>
-        </form>
-    </div>";
+    <div class=\"col-md-3 col-sm-6 col-xs-12\">
+                        <div class=\"card\">
+                            <img src=\"$md_image\" class=\"card-img\" alt=\"\">
+                            <div class=\"card-container\">
+                                <h5 class=\"card-title\">$md_name</h5>
+                                <p class=\"card-text\">$md_dept</p>
+                                <h4 class=\"card-text\">$md_loc</h4>
+                                <a href=\"doctor-profile.php?id='.$md_id.'\" class=\"btn btn-primary\">View Profile</a>
+                            </div>
+                        </div>
+                    </div>";
 
     echo $element;
 }
