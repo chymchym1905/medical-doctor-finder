8e38718d0b24c4b508c4c5761811fb774bf58781
M. Nart <tranquocduy184@gmail.com>
2022-12-08 14:17:34 +0700

    update something

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,4 @@
-<footer class = "footer-bg">
+<div id="footer" class = "footer-bg">
     <div class="container-fluid footer-about">
         <p class="float-end mb-1">
             <a href="#" style="color: var(--color-3)">Back to top </a>
@@ -7,7 +7,7 @@
             <div class="col-lg-4">
                 <h4>About</h4>
                 <div class="hline-w"></div>
-                <p><b>MDFinder</b><br>
+                <p><b>About This Webpage</b><br>
                     Founded on with a sense of purpose that make people find a good doctor easier  </p>
             </div>
             <div class="col-lg-4">
@@ -22,9 +22,9 @@
             <div class="col-lg-4">
                 <h4>Our Address</h4>
                 <div class="hline-w"></div>
-                <p><b>Main Office:</b>
+                <p><b>Showroom:</b>
                     268 Ly Thuong Kiet, Ward 14, <br> District 10, Ho Chi Minh City<br>
-                    <b>Branch Office:</b>
+                    <b>Branch:</b>
                     102, Ham Nghi, Ward 2<br> District 1, Ho Chi Minh City
                 </p>
             </div>
@@ -33,4 +33,4 @@
     <div id="copyright" class="text-center p-2">
         <h6>Copyright &copy 2022 Medical Doctor Finder</h6>
     </div>
-</footer>
+</div>
