b71f39c0da5398dec3097c2b45b8cc02e60069b0
WinstonTNguyen <winstontnguyen@gmail.com>
2022-12-08 14:21:52 +0700

    Update cards_display.php

--- libs/cards_display.php
+++ libs/cards_display.php
@@ -7,7 +7,7 @@ function card($md_id, $md_name, $md_dept, $md_image, $md_loc){
                             <img src=\"$md_image\" class=\"card-img\" alt=\"\">
                             <div class=\"card-container\">
                                 <h5 class=\"card-title\">$md_name</h5>
-                                <p class=\"card-text\">$md_dept</p>
+                                <p class=\"card-text\">Khoa $md_dept</p>
                                 <h4 class=\"card-text\">$md_loc</h4>
                                 <a href=\"doctor-profile.php?id='.$md_id.'\" class=\"btn btn-primary\">View Profile</a>
                             </div>
